5acffc2307edf49da46b7df8e00be76d0fbf5bb1
saifmhr <saifmahar165@gmail.com>
2022-10-21 18:27:09 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+public class Employee
+{
+    public string FirstName = "FN";
+    public string LastName = "LN";
+
+    public virtual void PrintFullName()
+    {
+        Console.WriteLine(FirstName + " " + LastName);
+    }
+}
+
+public class FullTimeEmployee:Employee
+{
+    public override void PrintFullName()
+    {
+        Console.WriteLine(FirstName + " " + LastName+" -Full Time");
+    }
+}
+
+public class PartTimeEmployee:Employee
+{
+    public override void PrintFullName()
+    {
+        Console.WriteLine(FirstName + " " + LastName + " -Part Time");
+    }
+
+}
+
+public class TemporaryEmployee : Employee
+{
+    public override void PrintFullName()
+    {
+        Console.WriteLine(FirstName + " " + LastName + " -Temporary");
+    }
+}
+
+ public class Program
+    {
+        static void Main()
+        {
+        Employee[] employees=new Employee[4];
+        employees[0] = new Employee();
+        employees[1]=new FullTimeEmployee();
+        employees[2]=new PartTimeEmployee();
+        employees[3]=new TemporaryEmployee();
+
+        foreach(Employee e in employees)
+        {
+            e.PrintFullName();
+        }
+
+        }
+    }
